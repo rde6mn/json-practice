89a4d51421e1fa7220d4ffc9e6fd16f79376b1ce
Mohini Gupta <rde6mn@virginia.edu>
2024-04-25 05:23:08 +0000

    bash script

---
+++ jq/lab8.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -s "https://aviationweather.gov/api/data/metar?ids=KMCI&format=json&taf=false&hours=12&bbox=40%2C-90%2C45%2C-85" > aviation.json
+cat aviation.json | jq -r .[].receiptTime | head -6
