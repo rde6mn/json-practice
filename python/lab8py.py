b9f0d67581a09dc9bc04f11594ecd7cbb28c758b
Mohini Gupta <rde6mn@virginia.edu>
2024-04-26 02:17:10 +0000

    python script

---
+++ python/lab8py.py
@@ -0,0 +1,12 @@
+import json
+import pandas as pd
+
+with open('data/schacon.repos.json', 'r') as file:
+    data = json.load(file)
+
+list1 = []
+for x in range(5):
+    list1.append([file[x]["name"], file[x]["html_url"],file[x]["updated_at"], file[x]["visibility"]])  
+
+df = pd.DataFrame(list1)
+df.to_csv('chacon.csv')
